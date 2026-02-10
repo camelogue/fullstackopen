d88db2c54fd8cce0e500e99f5814f6abe854a9a0
camelogue <yusufzahitmert@gmail.com>
2026-02-10 21:29:08 +0300

    Part 1: Exercise 1.11 - unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,10 @@ const Button = ({text, onClick}) => {
 
 const StatisticLine = ({text, value}) => {
   return (
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -23,14 +26,16 @@ const Statistics = ({good, neutral, bad}) => {
     )
   } else {
     return (
-      <>
-        <StatisticLine text='good' value={good} />
-        <StatisticLine text='neutral' value={neutral} />
-        <StatisticLine text='bad' value={bad} />
-        <StatisticLine text='all' value={all} />
-        <StatisticLine text='average' value={average} />
-        <StatisticLine text='positive' value={positive + ' %'} />  
-      </>
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='all' value={all} />
+          <StatisticLine text='average' value={average} />
+          <StatisticLine text='positive' value={positive + ' %'} />
+        </tbody>
+      </table>
     )
   }
 }
