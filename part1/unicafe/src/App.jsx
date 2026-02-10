c07044cd557819fe363996a49f9487d1348a7b3c
camelogue <yusufzahitmert@gmail.com>
2026-02-10 20:13:13 +0300

    Part 1: Exercise 1.6 - unicafe step 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const Button = ({text, onClick}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Display = ({text, value}) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increseGood = () => {
+    setGood(good + 1)
+  }
+  const increseNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const increseBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text='good' onClick={increseGood} />
+      <Button text='neutral' onClick={increseNeutral} />
+      <Button text='bad' onClick={increseBad} />
+      <h1>statistics</h1>
+      <Display text='good' value={good} />
+      <Display text='neutral' value={neutral} />
+      <Display text='bad' value={bad} />
+    </div>
+  )
+}
+
+export default App
