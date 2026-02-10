92fd72a345f6fb03a6e9d3ac73db8b4c6f1f3ce4
camelogue <yusufzahitmert@gmail.com>
2026-02-10 22:56:14 +0300

    Part 1: Exercise 1.14 - anecdotes step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -28,10 +28,13 @@ const App = () => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={voteAnecdote}>vote</button>
       <button onClick={changeSelected}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
     </div>
   )
 }
