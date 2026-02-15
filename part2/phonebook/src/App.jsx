13c5c3e43a8e6525deb921f6658775261ff4c2f3
camelogue <yusufzahitmert@gmail.com>
2026-02-16 01:31:18 +0300

    Part 2: Exercise 2.8 - The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
+    event.preventDefault()
     if (persons.findIndex(person => person.name === newName) !== -1) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      event.preventDefault()
-      const newPersons = [...persons, {name: newName}]
+      const newPersons = [...persons, {name: newName, number: newNumber}]
       setPersons(newPersons)
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -28,11 +33,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
