f2c47192538074ab1f80dfac23ac67e6fea3c969
camelogue <yusufzahitmert@gmail.com>
2026-02-26 13:37:41 +0300

    Part 2: Exercise 2.19 - Data for countries, step 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -9,6 +9,7 @@ import countryService from './services/countries'
 const App = () => {
   const [keyword, setKeyword] = useState('')
   const [countries, setCountries] = useState(null)
+  const [chosenCountry, setChosenCountry] = useState(null)
 
   useEffect(() => {
     countryService
@@ -24,12 +25,17 @@ const App = () => {
 
   const handleKeywordChange = (event) => {
     setKeyword(event.target.value)
+    setChosenCountry(null)
+  }
+
+  const handleShowCountry = (country) => {
+    setChosenCountry(country)
   }
 
   return (
     <div>
       find countries <Filter value={keyword} handleKeywordChange={handleKeywordChange} />
-      <ContentArea countries={countriesToShow} />
+      <ContentArea countries={chosenCountry ? [chosenCountry] : countriesToShow} onShow={handleShowCountry} />
     </div>
   )
 }
--- part2/countries/src/components/ContentArea.jsx
+++ part2/countries/src/components/ContentArea.jsx
@@ -1,12 +1,16 @@
 import CountryInfo from './CountryInfo'
 
-const ContentArea = ({countries}) => {
+const ContentArea = ({countries, onShow}) => {
     if (countries.length > 10) {
         return <p>Too many matches, specify another filter</p>
     } else if (countries.length > 1) {
         return (
             <div>
-                {countries.map(country => <p key={country.name.common}>{country.name.common}</p>)}
+                {countries.map(country => 
+                <div key={country.name.common}>
+                    {country.name.common} <button onClick={() => onShow(country)}>Show</button>
+                </div>
+                )}
             </div>
         )
     } else if (countries.length === 1) {
